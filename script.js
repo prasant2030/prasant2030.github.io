68782d12ba6692fa1a48f8e29a8081ff0848f051
Prasant Kumar Behera <119404952+prasant2030@users.noreply.github.com>
2025-08-02 13:39:38 +0530

    Add files via upload
    Signed-off-by: Prasant Kumar Behera <119404952+prasant2030@users.noreply.github.com>

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let isTyping = false;
 let typingIndex = 0;
 const typingTexts = [
     "Senior Chief Engineer | Cofounder | CEO",
-    "17+ years of experience at Samsung R&D, India & IntuitiveCoder",
+    "17+ years of experience at Samsung R&D Institute, India & IntuitiveCoder",
     "Pioneering execution excellence across global teams",
     "99.98% on-time delivery rate with zero defects"
 ];
